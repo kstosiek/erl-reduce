9fa6dde1f0bb2a3b3dea27936c3d9b1fd81acd07
Karol Stosiek <karol.stosiek@gmail.com>
2010-12-28 14:18:06 +0100

    Basic tests for master, reduce_worker and map_worker

--- src/master.erl
+++ src/master.erl
@@ -6,8 +6,12 @@
 
 %%
 %% Exported Functions.
-%%
--export([run/3]).
+%% TODO: move partition_map_data/2 to a separate module.
+-export([run/3,
+         partition_map_data/2,
+         execute_map_phase/2, % exported for testing purposes.
+         execute_reduce_phase/2 % exported for testing purporses.
+        ]).
 
 
 %%
@@ -15,13 +19,16 @@
 %%
 
 %% @doc Main master function. Executes map/reduce operation on given input
-%%     with given map and reduce workers. Returns overall result.
+%%     with given map and reduce workers. Returns overall result. We require
+%%     to have at least one map and one reduce worker pid available.
 %% @spec (MapWorkerPids, ReduceWorkerPids, InputData) -> FinalResult where
 %%    MapWorderPids = [pid()],
 %%    ReduceWorkerPids = [pid()],
 %%    InputData = [{K1,V1}],
 %%    FinalResult = [{K3,V3}]
-run(MapWorkerPids, ReduceWorkerPids, InputData) ->   
+run(MapWorkerPids, ReduceWorkerPids, InputData)
+  when length(MapWorkerPids) > 0,
+       length(ReduceWorkerPids) > 0 ->   
     MapResult = execute_map_phase(InputData, MapWorkerPids),
     ReduceResult = execute_reduce_phase(MapResult, ReduceWorkerPids),
     ReduceResult.
@@ -30,31 +37,35 @@ run(MapWorkerPids, ReduceWorkerPids, InputData) ->
 %% Local Functions.
 %%
 
-%% @doc Partitions given Data into chunks of at most SliceSize and appends
+%% @doc Partitions given Data into chunks of at most ChunkSize and appends
 %%     resulting chunk to the Accumulator. If there is at least ChunkSize
 %%     elements in the given list, then chunk size is ChunkSize; otherwise
 %%     there is at most as many elements in the chunk, as in the given list.
 %%     TODO: make the difference in chunk sizes be at most 1 element.
-%% @spec (Data,SliceSize,Accumulator) -> PartitionedMapData where
+%% @spec (Data,ChunkSize,Accumulator) -> PartitionedMapData where
 %%     Data = [{K1,V1}],
-%%     SliceSize = int(),
+%%     ChunkSize = int(),
 %%     Accumulator = PartitionedMapData = [Data]
 partition_data_with_accumulator([], _, Accumulator) ->
     Accumulator;
 
+partition_data_with_accumulator(Data, ChunkSize, Accumulator)
+  when length(Data) < ChunkSize ->
+    Accumulator;
+
 partition_data_with_accumulator(Data, ChunkSize, Accumulator) ->
-    {Chunk, OtherData} = lists:split(erlang:min(ChunkSize, length(Data)), Data),
+    {Chunk, OtherData} = lists:split(ChunkSize, Data),
     partition_data_with_accumulator(OtherData, ChunkSize, [Chunk|Accumulator]).
 
 
 %% @doc Parititions data into almost evenly-sized data chunks. Last chunk may
 %%     not be as big as other chunks.
 %%     TODO: make the difference in chunk sizes be at most 1 element.
-%% @spec (Data,Slices) -> [Data] where
+%% @spec (Data,Chunks) -> [Data] where
 %%     Data = [{K1,V1}],
-%%     Slices = int()
-partition_map_data(Data, Slices) ->
-    partition_data_with_accumulator(Data, round(length(Data) / Slices), []).
+%%     Chunks = int()
+partition_map_data(Data, Chunks) ->
+    partition_data_with_accumulator(Data, round(length(Data) / Chunks), []).
 
 %% @doc Collects results from given map workers (MapWorkerPids)
 %%     into CollectedResults (accumulator); RemainingWorkers contains
@@ -70,8 +81,8 @@ collect_map_results_loop(_, CollectedResults, 0) ->
 
 collect_map_results_loop(MapWorkerPids,
                          CollectedResults,
-                         RemainingWorkers) ->
-    receive 
+                         RemainingWorkers) when RemainingWorkers > 0 ->
+    receive
         {MapWorkerPid, {map_result, Result}} ->
             collect_map_results_loop(MapWorkerPids, [Result|CollectedResults],
                                      RemainingWorkers - 1)
@@ -83,7 +94,7 @@ collect_map_results_loop(MapWorkerPids,
 %% @spec (MapWorkerPids) -> IntermediateData where
 %%     MapWorkerPids = [pid()],
 %%     IntermediateData = [{K2,V2}]
-collect_map_results(MapWorkerPids) ->
+collect_map_results_1(MapWorkerPids) ->
     collect_map_results_loop(MapWorkerPids, [], length(MapWorkerPids)).
 
 
@@ -92,16 +103,16 @@ collect_map_results(MapWorkerPids) ->
 %%     MapData = [{K1,V1}],
 %%     MapWorkerPids = [pid()],
 %%     IntermediateData = [{K2,V2}]
+%% @private
 execute_map_phase(MapData, MapWorkerPids) ->
     MapDataParts = partition_map_data(MapData, length(MapWorkerPids)),
     
     %% Spread data among the map workers.
-    case lists:foreach(lists:zip(MapWorkerPids, MapDataParts)) of
-        {MapWorkerPid, MapDataPart} ->
-            MapWorkerPid ! {self(), {map_data, MapDataPart}}
-    end,
-    
-    MapResult = collect_map_results(MapWorkerPids),
+    lists:foreach(fun({MapWorkerPid, MapDataPart}) ->
+                          MapWorkerPid ! {self(), {map_data, MapDataPart}}
+                  end,
+                  lists:zip(MapWorkerPids, MapDataParts)),
+    MapResult = collect_map_results_1(MapWorkerPids),
     MapResult.
 
 
@@ -110,6 +121,7 @@ execute_map_phase(MapData, MapWorkerPids) ->
 %%     ReduceData = [{K2,V2}],
 %%     ReduceWorkerPids = [pid()],
 %%     FinalResult = [{K3,V3}]
+%% @private
 execute_reduce_phase(ReduceData, ReduceWorkerPids) ->
     ReduceResult = ReduceData,
     %% TODO: implement reduce phase.
