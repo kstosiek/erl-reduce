269f2afdcf9e4986f377bdda44da953a2d9119f0
poles-p <poles_p@wp.pl>
2010-12-29 23:20:45 +0800

    reduce_worker implementation and simple test.

--- tests/reduce_worker_tests.erl
+++ tests/reduce_worker_tests.erl
@@ -16,3 +16,19 @@ reduce_phase_successful_computation_test() ->
     ActualResult = master:execute_reduce_phase(ReduceData, []),
     ExpectedResult = ReduceData,
     ?assertEqual(ExpectedResult, ActualResult).
+
+% Simple reduce_worker test
+reduce_worker_test() ->
+	ReduceData = [{1,"a"}],
+	ExpectedResult = ["a"],
+	ReduceWorkerPid = spawn(reduce_worker, run, 
+							[fun({_,V}) -> V end]),
+	ReduceWorkerPid ! {self(), {reduce_data, ReduceData}},
+	receive
+		{_, reduce_data_acknowledged} ->
+			ReduceWorkerPid ! {self(), start_reducing},
+			receive
+				{_, {reduce_finished, ActualResult}} ->
+					?assertEqual(ExpectedResult, ActualResult)
+			end
+	end.
